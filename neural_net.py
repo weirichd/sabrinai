ac1a964c7a84cabcf4a4817f571bdbac1dda1e48
David Edward Weirich <weirich.david@gmail.com>
2019-02-13 20:25:59 -0500

    Finished setting up

--- neural_net.py
+++ neural_net.py
@@ -1,70 +1,94 @@
+#############################################
 #
-# Run this after running get_scripts.py
+# Run this after running get_episodes.py
 #
-
-#
-# Run this after running get_scripts.py
-#
+#############################################
 
 import numpy as np
+
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
-from tensorflow.keras.layers import Dropout
 from tensorflow.keras.layers import LSTM
-from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.callbacks import ModelCheckpoint, LambdaCallback
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.layers import Embedding, Activation, Softmax
 
 import os
 
-from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, LambdaCallback
+
+def clean_episode_text(text, header_length=0):
+    """
+    Clean the text of an episode.
+
+    :param text:
+    :param header_length:
+    :return:
+    """
+
+    # Remove any leading newlines or whitespaces at the beginning and end of the episode text.
+    text = text.strip()
+
+    # Insert a space before these punctuation marks so they get treated as a word.
+    for c in '.!?),':
+        text = text.replace(c, ' ' + c)
+
+    # Treat these two a special cases
+    text = text.replace(')', ' )')
+    text = text.replace('\n', ' \n ')
+
+    # If the "cleaning" I just did put two or more spaces next to each other, get rid of that.
+    while '  ' in text:
+        text = text.replace('  ', ' ')
+
+    text = ('episodebeginning ' * header_length) + text + (' episodeend' * header_length)
+
+    return text
 
 
-def clean_text(t):
-    t = t.strip()
-    for c in '.!?(),':
-        t = t.replace(c, ' ' + c)
-    t = t.replace('\n', ' \n ')
+def load_data(episodes_path='episodes', sequence_length=32):
+    texts = [clean_episode_text(open(os.path.join(episodes_path, f)).read()) for f in os.listdir(episodes_path)]
 
-    return t
+    tkn = Tokenizer(num_words=50000, filters='"#$%&*+-/:;<=>@[\\]^_`{|}~\t')
 
+    tkn.fit_on_texts(texts)
 
-episodes_path = 'episodes'
-sequence_length = 32
+    seqs = tkn.texts_to_sequences(texts)
 
-texts = [clean_text(open(os.path.join(episodes_path, f)).read()) for f in os.listdir(episodes_path)]
+    sub_texts = []
+    for seq in seqs:
+        sub_texts += [
+            (seq[i:i+sequence_length], seq[i+sequence_length], [j/len(seq) for j in range(i, i+sequence_length)])
+            for i in range(len(seq) - sequence_length)
+        ]
 
-tkn = Tokenizer(num_words=50000, filters='"#$%&*+-/:;<=>@[\\]^_`{|}~\t')
+    X = np.array([s[0] for s in sub_texts])
+    y = np.array([s[1] for s in sub_texts])
 
-tkn.fit_on_texts(texts)
-
-seqs = tkn.texts_to_sequences(texts)
-
-vocab_size = len(tkn.word_index) + 1
-
-short_sequences = []
-for seq in seqs:
-    short_sequences += [(seq[i:i+sequence_length], seq[i+sequence_length])
-                        for i in range(len(seq) - sequence_length )]
-
-X = [a[0] for a in short_sequences]
-y = [a[1] for a in short_sequences]
-
-model = Sequential()
-model.add(Embedding(vocab_size, 128, input_length=32))
-model.add(LSTM(128))
-model.add(Dense(128))
-model.add(Activation('relu'))
-model.add(Dense(vocab_size))
-model.add(Softmax())
-
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy')
-
-X = np.array(X)
-y = np.array(y)
+    return X, y, tkn
 
 
-def random_sentence(sentence_len=25):
+X, y, tokenizer = load_data()
+vocab_size = len(tokenizer.word_index) + 1
+
+
+def build_mode(vocab_size):
+    model = Sequential()
+    model.add(Embedding(vocab_size, 128, input_length=32))
+    model.add(LSTM(128))
+    model.add(Dense(128))
+    model.add(Activation('relu'))
+    model.add(Dense(vocab_size))
+    model.add(Softmax())
+
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy')
+
+    return model
+
+
+model = build_mode(vocab_size)
+
+
+def random_sentence(tkn, sentence_len=25):
     arr = np.random.randint(0, vocab_size, size=32)
     result = []
 
@@ -79,19 +103,18 @@ def random_sentence(sentence_len=25):
     for c in '.!?(),':
         sentence = sentence.replace(' ' + c, c)
 
-    print()
-    print(sentence)
-    print()
+    sentence = sentence.replace('\n', ' \n ')
+
+    return sentence
 
 
-callbacks=[LambdaCallback(on_epoch_end=lambda epoch, logs: random_sentence(20))]
+callbacks = [LambdaCallback(on_epoch_end=lambda epoch, logs: print('\n', random_sentence(tokenizer, 50), '\n'))]
 
-
-model.fit(X[:X.shape[0]], y[:X.shape[0]],
-          epochs=10,
+model.fit(X[:100000], y[:100000],
+          epochs=32,
           batch_size=100,
           callbacks=callbacks)
 
-model.save('sabrinai.hdf5')
+model.save('sabrinai_2.hdf5')
 
-random_sentence(1000)
+print(random_sentence(tokenizer, 1000))
